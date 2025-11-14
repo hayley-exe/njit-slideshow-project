63582e23f1b62b00aad1619d807e5c94131a4b85
hayley-exe <526hslater@frhsd.com>
2025-11-14 11:04:32 -0500

    finish images soon

--- gallery.js
+++ gallery.js
@@ -3,7 +3,7 @@ let mImages = [];
 const mWaitTime = 5000;
 let timer = null;
 
-// Exact JSON data provided
+// Complete JSON with all images and consistent imgLocation fields
 const sampleJSON = {
   "images": [
     {
@@ -18,18 +18,22 @@ const sampleJSON = {
     },
     {
       "imgPath": "img/vibes/trader-mountain.jpeg",
+      "imgLocation": "Wandering Trader in Mountains",
       "biome": "Windswept Hills"
     },
     {
       "imgPath": "img/vibes/sunflower.png",
+      "imgLocation": "Sunflower Fields",
       "biome": "Sunflower Plains"
     },
     {
       "imgPath": "img/vibes/plains.png",
+      "imgLocation": "Open Plains",
       "biome": "Plains"
     },
     {
       "imgPath": "img/vibes/fields.png",
+      "imgLocation": "Flower Fields",
       "biome": "Flower Forest"
     },
     {
@@ -39,14 +43,52 @@ const sampleJSON = {
     },
     {
       "imgPath": "img/vibes/beach-water.png",
+      "imgLocation": "Sandy Beach",
       "biome": "Beach"
     },
     {
       "imgPath": "img/vibes/magic-forest.jpeg",
+      "imgLocation": "Enchanted Dark Forest",
       "biome": "Dark Oak Forest"
     },
     {
+      "imgPath": "img/vibes/taiga.png",
+      "imgLocation": "Snowy Taiga Village",
+      "biome": "Taiga"
+    },
+    {
+      "imgPath": "img/vibes/birch-forest.png",
+      "imgLocation": "Birch Forest Hills",
+      "biome": "Birch Forest"
+    },
+    {
+      "imgPath": "img/vibes/swamp.png",
+      "imgLocation": "Witch Hut Swamp",
+      "biome": "Swamp"
+    },
+    {
+      "imgPath": "img/vibes/eroded-bad.png",
+      "imgLocation": "Eroded Badlands Canyon",
+      "biome": "Eroded Badlands"
+    },
+    {
+      "imgPath": "img/vibes/plateau-bad.png",
+      "imgLocation": "Badlands Plateau",
+      "biome": "Badlands Plateau"
+    },
+    {
+      "imgPath": "img/vibes/river.png",
+      "imgLocation": "River Valley",
+      "biome": "River"
+    },
+    {
+      "imgPath": "img/vibes/ocean.png",
+      "imgLocation": "Ocean Monument",
+      "biome": "Ocean"
+    },
+    {
       "imgPath": "img/vibes/lush.jpeg",
+      "imgLocation": "Lush Cave Entrance",
       "biome": "Lush Caves"
     },
     {
@@ -56,15 +98,33 @@ const sampleJSON = {
     },
     {
       "imgPath": "img/vibes/jungle-edge.png",
+      "imgLocation": "Jungle Edge Transition",
       "biome": "Jungle Edge"
     },
     {
       "imgPath": "img/vibes/jungle.png",
+      "imgLocation": "Dense Jungle",
       "biome": "Jungle"
     },
     {
       "imgPath": "img/vibes/bamboo.png",
+      "imgLocation": "Bamboo Jungle",
       "biome": "Bamboo Forest"
+    },
+    {
+      "imgPath": "img/vibes/frozen-river.png",
+      "imgLocation": "Frozen River",
+      "biome": "Frozen River"
+    },
+    {
+      "imgPath": "img/vibes/frozen-ocean.png",
+      "imgLocation": "Deep Frozen Ocean",
+      "biome": "Deep Frozen Ocean"
+    },
+    {
+      "imgPath": "img/vibes/ice-spikes.png",
+      "imgLocation": "Ice Spikes Biome",
+      "biome": "Ice Spikes"
     },
     {
       "imgPath": "img/vibes/mushroom-fields.png",
@@ -73,13 +133,28 @@ const sampleJSON = {
     },
     {
       "imgPath": "img/vibes/warped-forest.png",
-      "imgLocation": "Nether",
+      "imgLocation": "Warped Forest",
       "biome": "Warped Forest"
     },
     {
       "imgPath": "img/vibes/crimson-forest.png",
-      "imgLocation": "Nether",
+      "imgLocation": "Crimson Forest",
       "biome": "Crimson Forest"
+    },
+    {
+      "imgPath": "img/vibes/nether-wastes.png",
+      "imgLocation": "Nether Fortress",
+      "biome": "Nether Wastes"
+    },
+    {
+      "imgPath": "img/vibes/soul-sand.png",
+      "imgLocation": "Soul Sand Valley",
+      "biome": "Soul Sand Valley"
+    },
+    {
+      "imgPath": "img/vibes/basalt-delta.png",
+      "imgLocation": "Basalt Deltas",
+      "biome": "Basalt Delta"
     }
   ]
 };
@@ -87,7 +162,7 @@ const sampleJSON = {
 $(document).ready(() => {
   $('.details').hide();
 
-  // Load images from exact JSON
+  // Load all images
   mImages = sampleJSON.images;
   swapPhoto(); // Show first image
   startTimer();
@@ -107,17 +182,19 @@ function swapPhoto() {
   if (mImages.length === 0) return;
 
   const img = mImages[mCurrentIndex];
-  $('#photo').attr('src', img.imgPath);
+  $('#photo').attr('src', img.imgPath).on('load', function () {
+    $(this).addClass('loaded');
+  });
 
-  // Only show location if imgLocation exists
+  // Show location if exists, otherwise hide
   if (img.imgLocation) {
-    $('.location').text(`Location: ${img.imgLocation}`);
+    $('.location').text(`Location: ${img.imgLocation}`).show();
   } else {
-    $('.location').text('');
+    $('.location').hide();
   }
 
-  // Always show biome as description
-  $('.description').text(`Biome: ${img.biome}`);
+  // Always show biome
+  $('.description').text(`${img.biome}`);
 }
 
 function showNextPhoto() {
@@ -139,5 +216,5 @@ function startTimer() {
 
 function restartTimer() {
   clearInterval(timer);
-  timer = setInterval(showNextPhoto, mWaitTime);
+  startTimer();
 }
